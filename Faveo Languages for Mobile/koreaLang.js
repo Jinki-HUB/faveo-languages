4b59c9925b2ec98adc77723f6a97b5735c142041
Jinki-HUB <laislavonita@gmail.com>
2021-09-19 22:03:21 +0900

    Create koreaLang.js

---
+++ Faveo Languages for Mobile/koreaLang.js
@@ -0,0 +1,120 @@
+export default {
+  /* User login Page */
+  SINGIN: "로그인",
+  REMEMBERME: "나를 기억",
+  USERNAME: "사용자 이름",
+  PASSWORD: "비밀번호",
+  LOGIN: "로그인",
+  LDAP: "LDAP를 통해 로그인",
+  FIELDVALID: "이 필드는 필수입니다",
+  SEND: "로그인",
+  LOGINDISCT: "세션을 시작하려면 로그인 하십시오",
+  CREATEACCOUNT: "등록",
+  CHANGELANGAUGE: "언어 변경",
+  EMAILUSERNAME: "메일/사용자 이름",
+  /* Flash Screen page */
+  FLASHTITLE: "고객 지원 간소화",
+  /* 2fa Screen page */
+  TWOFADISCRIPTION:
+    "장치에서 2단계 인증 앱을 열어 인증 코드를 보고 ID 확인하십시오.",
+  TWOFATITLE: "2단계 인증",
+  TWOFAENTERCODE: "구글 인증 프로그램 코드 입력",
+  /* Update Email Screen page */
+  UPDATEEMAIL: "메일 업데이트",
+  ALREADYACTIVATED: "이미 활성된 계정",
+  /* Verify Email Screen page */
+  VERIFYCONTAINONE: "안녕하십니까!",
+  VERIFYCONTAINTWO: "활성 링크를 보냈습니다",
+  VERIFYCONTAINTHREE:
+    ". 메일함을 확인하고 링크를 클릭하여 계정을 활성한 다음 다시 시도하십시오.",
+  RESENDLINK: "활성 링크 재전송",
+  VERIFYOR: "또는",
+  VERIFYEMAILUPDATE: "메일 업데이트",
+  /* OOPS Screen page */
+  OOPS: "시스템 관리자와 연결하십시오",
+  /* Inbox Screen page */
+  NOTICKETFOUND: "티켓을 찾을 수 없습니다.",
+  TICKET: "티켓",
+  LOADING: "로딩 중",
+  /* Drawer Screen page */
+  CREATETICKET: "티켓 생성",
+  MYTICKET: "내 티켓",
+  PERSONALINFO: "개인적인 정보",
+  ABOUTUS: "회사 소개",
+  SINGOUT: "로그 아웃",
+  LOGOUT: "로그 아웃",
+  LOGOUTMASSAGE: "로그 아웃 하시겠습니까?",
+  SEARCHTICKET: "티켓 검색",
+  RENEWAL: "갱신 계획",
+  CONFORM: "확인",
+  /* Create Ticket Screen page */
+  CREATETICKET: "티켓 생성",
+  RETAKE: "재촬영",
+  USEPHOTO: "사진 사용",
+  SEE: "나를 보려면 클릭",
+  ATTACHED: "첨부 파일",
+  DONE: "완료",
+  NOMATCHFOUNDS: "일치하는 항목이 없습니다.",
+  CANCEL: "취소",
+  CLICKHERESEECC: "추가된 숨은참조를 보려면 여기를 클릭: ",
+  ADDCC: "Add CC",
+  DATEFORMATDISCRIPTION: "날짜는 DD/MM/YYYY 형식이어야 합니다",
+  EMAILFORMATDISCRIPTION: "유효한 메일 주소를 입력하십시오",
+  EMAILFORMAT: "메일 주소를 입력하십시오",
+  ADDEDCC: "추가된 숨은참조",
+  NUMBERDISCRIPTION: "입력은 숫자로만 구성되어야 합니다",
+  ENTERTHEVALUE: "값을 입력",
+  SEARCH: "검색 중...",
+  SELECT: "선택",
+  SEARCHDATE: "날짜 선택",
+  ORGANIZATION: "조직 생성",
+  /* Ticket Thread Screen page */
+  DOWNLOAD: "다운로드",
+  TAPREPLY: "답변",
+  SELECTLANGUAGE: "프로필 세부사항",
+  /* User Registation page */
+  REGISTATIONTITLE: "등록",
+  CREATEAUSER : "사용자 생성",
+  SUBMIT: "제출",
+  PROFILEEDIT: "프로필 편집",
+  CHOOSELANGUAGE: "언어 선택",
+  UPDATEPROFILE: "프로필 업데이",
+  /* Forgot Password page */
+  WEKNOWPASSWORD: "내 비밀번호를 알고 있습니다",
+  CHANGEPASSWORD: "전송",
+  FORGOTPASSWORD: "비밀번호를 잊어 버렸습니다",
+  CPASSWORD: "비밀번호 변경",
+  UPDATE: "업데이트",
+  /* Every Page */
+  APIDISABLED: "API 현재 비활성 입니다.",
+  INTERNETERROR: "인터넷 연결을 확인하십시오.",
+  RETRY: "재시도",
+  LANGUAGEPRE: "선호하는 언어를 선택하십시오",
+  /* reply page */
+  LEAVEREPLY: "답변을 남겨주십시오",
+  /* Login Url page */
+  HELPDESK: "헬프데스크 Url",
+  PLACEHOLDER: "https://support...",
+  NEED_HELP: "헬프데스크 URL에 대한 도움이 필요하십니까?",
+  /* HelpLink Component */
+  WELCOME: "헬프데스크 URL이 무엇인지 궁금하십니까?",
+  INSTRUCTIONS:
+    "헬프 데스크가 설치된 URL입니다. 자세한 내용은 IT 관리자에게 문의하십시오.",
+  FOOTER: "자세한 내용은 support@ladybirdweb.com 로 메일을 보내주십시오",
+  EDITTICKET: "티멧 편집",
+  /* Notification Component */
+  NOTIFICATIONTITLE: "통보",
+  NONOTIFICATIONFOUND: "통보를 찾을 수 없습니다.",
+  /* About Screen page */
+  FAVEOSUPPORT: "Faveo 지원",
+  VERSION: "버전 7.0.0",
+  ABOUTUSDISCRIPTION:
+    "방갈로르에 본사를 둔 Faveo 헬프데스크는 기업들에게 고객 지원을 관리하기 위한 자동화된 헬프데스크 시스템을 제공합니다. Faveo 단어는 호의적이라는 의미의 라틴어에서 유래했습니다. 이는 Faveo가 가진 제품의 기능뿐만 아니라 비전과 범위를 진정으로 강조합니다. 그것은 특히 신생기업과 중소기업이 최첨단 티켓 기반 지원 시스템으로 그들에게 힘을 실어주기 위해 고안되었습니다. 오늘날의 경쟁력 있는 시작 시나리오에서는 고객 유지 문제가 주요 과제 중 하나입니다. 고객 쿼리를 부지런히 처리하는 것은 오래 지속되는 관계를 유지하는 것과 잃는 것의 차이입니다. 회사는 전략적 통찰력을 위한 소비자 질의 관리 도구를 제공하고 기업이 이러한 결정적인 결정을 내릴 수 있도록 지원하는 열정을 가지고 있습니다.",
+  WEBSITE: "웹사이트",
+  WEBDETAIL: "www.faveohelpdesk.com",
+  TITLE: "회사 소개",
+  COMPANY_TITLE: "FAVEO 헬프데스크",
+  LINK: "www.faveohelpdesk.com",
+  BUTTON: "웹사이트",
+  CCLIST: "숨은참조 목록",
+};
